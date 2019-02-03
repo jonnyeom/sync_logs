baae9ef9f5dc75d6f9596a72a14d9c638048d782
Jonathan Eom <jeom@unleashed-technologies.com>
2019-02-02 22:45:59 -0500

    Move query blocks into its own component

--- src/components/ContentTypeSelector.js
+++ src/components/ContentTypeSelector.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 class ContentTypeButton extends React.Component {
   onSelect = event => {
-    console.log(event);
     this.props.onSelect(this.props.content_type);
   };
 
@@ -21,6 +20,21 @@ class ContentTypeButton extends React.Component {
   }
 }
 
+const ContentTypeQueries = (props) => {
+  // @TODO: Make this a Class and Loop over all Queries to make dynamic buttons.
+  return (
+    <div className="col-sm-6 col-lg-3">
+      <div className="card p-3">
+        <div className="d-flex align-items-center">
+          <div>
+            <h4 className="m-0"><a href="javascript:void(0)">{props.name} <small>@TODO</small></a></h4>
+            <small className="text-muted">Description</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
 
 class ContentTypeSelector extends React.Component {
 
@@ -28,81 +42,15 @@ class ContentTypeSelector extends React.Component {
     return (
       <div className="row">
         <ContentTypeButton label='All' content_type='' onSelect={this.props.onSelect}/>
-        <ContentTypeButton label='Event' content_type='event' onSelect={this.props.onSelect}/>
+        <ContentTypeButton label='Person' content_type='person' onSelect={this.props.onSelect}/>
+        <ContentTypeButton label='Product' content_type='product' onSelect={this.props.onSelect}/>
         <ContentTypeButton label='Course Item' content_type='course_item' onSelect={this.props.onSelect}/>
-        <div className="col-6 col-sm-4 col-lg-2">
-          <div className="card">
-            <div className="card-body p-3 text-center">
-              <div className="text-right text-green">
-                6%
-              </div>
-              <div className="h1 m-0">43</div>
-              <div className="text-muted mb-4">New Tickets</div>
-            </div>
-          </div>
-        </div>
-        <div className="col-6 col-sm-4 col-lg-2">
-          <div className="card">
-            <div className="card-body p-3 text-center">
-              <div className="text-right text-green">
-                6%
-              </div>
-              <div className="h1 m-0">43</div>
-              <div className="text-muted mb-4">New Tickets</div>
-            </div>
-          </div>
-        </div>
-        <div className="col-6 col-sm-4 col-lg-2">
-          <div className="card">
-            <div className="card-body p-3 text-center">
-              <div className="text-right text-green">
-                6%
-              </div>
-              <div className="h1 m-0">43</div>
-              <div className="text-muted mb-4">New Tickets</div>
-            </div>
-          </div>
-        </div>
-        <div className="col-sm-6 col-lg-3">
-          <div className="card p-3">
-            <div className="d-flex align-items-center">
-              <div>
-                <h4 className="m-0"><a href="javascript:void(0)">Query 1 <small>Sales</small></a></h4>
-                <small className="text-muted">12 waiting payments</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-sm-6 col-lg-3">
-          <div className="card p-3">
-            <div className="d-flex align-items-center">
-              <div>
-                <h4 className="m-0"><a href="javascript:void(0)">Query 2 <small>Sales</small></a></h4>
-                <small className="text-muted">12 waiting payments</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-sm-6 col-lg-3">
-          <div className="card p-3">
-            <div className="d-flex align-items-center">
-              <div>
-                <h4 className="m-0"><a href="javascript:void(0)">132 <small>Sales</small></a></h4>
-                <small className="text-muted">12 waiting payments</small>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-sm-6 col-lg-3">
-          <div className="card p-3">
-            <div className="d-flex align-items-center">
-              <div>
-                <h4 className="m-0"><a href="javascript:void(0)">132 <small>Sales</small></a></h4>
-                <small className="text-muted">12 waiting payments</small>
-              </div>
-            </div>
-          </div>
-        </div>
+        <ContentTypeButton label='Session' content_type='session' onSelect={this.props.onSelect}/>
+        <ContentTypeButton label='Event' content_type='event' onSelect={this.props.onSelect}/>
+        <ContentTypeQueries name='Query 1'/>
+        <ContentTypeQueries name='Query 2'/>
+        <ContentTypeQueries name='Query 3'/>
+        <ContentTypeQueries name='Query 4'/>
       </div>
     );
   }
